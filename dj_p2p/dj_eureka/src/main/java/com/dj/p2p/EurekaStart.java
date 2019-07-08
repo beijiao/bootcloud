974aaa7fe6a69346c6e5e5ee60123938b3ed0c65
yinyw <17631336417@163.com>
2019-07-08 10:55:26 +0800

    加注释

--- dj_p2p/dj_eureka/src/main/java/com/dj/p2p/EurekaStart.java
+++ dj_p2p/dj_eureka/src/main/java/com/dj/p2p/EurekaStart.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaStart {
-
+// 北京
     public static void main(String[] args) {
         SpringApplication.run(EurekaStart.class, args);
     }
